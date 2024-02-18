73b01ab5dd5a9abbb376cd81119d750c18dd27e6
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-02-18 13:25:38 +0530

    githubActions 08

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <div className="sky">
           <div className="text">
             <img src="./logo.png" alt="" />
-            <span>#githubActions 07</span>
+            <span>#githubActions 08</span>
           </div>
           <div className="stars"></div>
           <div className="stars1"></div>
